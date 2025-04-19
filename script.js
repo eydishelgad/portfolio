837ec2676504d068ec704b84bbe05f02cd9eee5e
eydishelgad <160533412+eydishelgad@users.noreply.github.com>
2025-04-18 19:07:00 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// Smooth scroll to sections
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      const target = document.querySelector(this.getAttribute('href'));
+      if (target) {
+        target.scrollIntoView({
+          behavior: 'smooth'
+        });
+      }
+    });
+  });
+  
